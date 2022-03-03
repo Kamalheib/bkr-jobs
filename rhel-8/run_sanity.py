d7d9980773cfd1b7f74f68df03a28342da0eb858
Kamal Heib <kheib@redhat.com>
2022-03-03 15:42:06 +0200

    bkr-jobs: Add rdma sanity jobs for RHEL-8
    Signed-off-by: Kamal Heib <kheib@redhat.com>

---
+++ rhel-8/run_sanity.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+
+job_list = [
+"bnxt-roce.xml",
+"cxgb4-iw.xml",
+"hfi1-opa.xml",
+"mlx4-ib.xml",
+"mlx4-roce.xml",
+"mlx5-ib.xml",
+"mlx5-roce.xml",
+"qedr-iw.xml",
+"irdma-roce.xml"
+]
+
+whit_board = ""
+distro_name = ""
+kernel_repo = "#package_name=kernel"
+
+for job in job_list:
+    os.systen("git checkout -- %s" % job)
+    os.system("sed -i 's,| RDMA sanity <,| RDMA sanity | %s<,g' %s" % (whit_board, job))
+    os.system("sed -i 's,<distro_name op=\"=\" value=\"\"/>,<distro_name op=\"=\" value=\"%s\"/>,g' %s" % (distro_name, job))
+    os.system("sed -i 's,<param name=\"KPKG_URL\" value=\"\"/>,<param name=\"KPKG_URL\" value=\"%s\"/>,g' %s" % (kernel_repo, job))
+    os.system("bkr job-submit %s" % (job))
